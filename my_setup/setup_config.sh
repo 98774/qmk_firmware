196f697aed7520e7aabfec6314bf70b96403b177
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-03-01 13:09:21 -0500

    Updated config names

---
+++ my_setup/setup_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Configures default settings for my handwired dactyl no RGB)
+qmk config user.name='98774'
+qmk config user.qmk_home=~/projects/qmk_firmware
+qmk config flash.keyboard=handwired/dactyl_manuform/4x5
+qmk config flash.keymap=default
+qmk config user.keyboard=handwired/dactyl_manuform/4x5
+qmk config user.keymap=default
