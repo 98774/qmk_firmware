073545ad50157a1f9ea10fc92ef568830293c707
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-03-01 13:27:28 -0500

    Added custom keymap to4x5 manuform

--- my_setup/setup_config.sh
+++ my_setup/setup_config.sh
@@ -4,6 +4,6 @@
 qmk config user.name='98774'
 qmk config user.qmk_home=~/projects/qmk_firmware
 qmk config flash.keyboard=handwired/dactyl_manuform/4x5
-qmk config flash.keymap=default
+qmk config flash.keymap=98774-noRGB
 qmk config user.keyboard=handwired/dactyl_manuform/4x5
-qmk config user.keymap=default
+qmk config user.keymap=98774-noRBG
