c0af6b40befdff99f7abf5dd1ee033e7f3c27ccf
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-03-27 20:56:48 -0400

    [Update] RGB_MATRIX: initial configuration for rgb_matrix

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/98774-RBG/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/98774-RBG/config.h
@@ -21,12 +21,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* Select hand configuration */
-//#define MASTER_LEFT
-#define MASTER_RIGHT
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 
 //#define EE_HANDS
+
+//Found on reddit
+//#define SPLIT_USB_DETECT
 
 // #define WS2812_DI_PIN D3
 // #define RGB_MATRIX_LED_COUNT 50
 
-#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING + 1
+// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING + 2
+// #define USB_MAX_POWER_CONSUMPTION 500
+
+#  define RGB_MATRIX_HUE_STEP 8
+#  define RGB_MATRIX_SAT_STEP 8
+#  define RGB_MATRIX_VAL_STEP 8
+#  define RGB_MATRIX_SPD_STEP 10
+
+#ifdef RGB_MATRIX_ENABLE
+// Enable animations
+// #  define ENABLE_RGB_MATRIX_ALPHAS_MODS                 // Static dual hue speed is hue for secondary hue
+// #  define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN            // Static gradient top to bottom speed controls how much gradient changes
+#  define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT         // Static gradient left to right speed controls how much gradient changes
+#  define ENABLE_RGB_MATRIX_BREATHING                   // Single hue brightness cycling animation
+#  define ENABLE_RGB_MATRIX_BAND_SAT                    // Single hue band fading saturation scrolling left to right
+// #  define ENABLE_RGB_MATRIX_BAND_VAL                    // Single hue band fading brightness scrolling left to right
+// #  define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT           // Single hue 3 blade spinning pinwheel fades saturation
+// #  define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL           // Single hue 3 blade spinning pinwheel fades brightness
+// #  define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT             // Single hue spinning spiral fades saturation
+// #  define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL             // Single hue spinning spiral fades brightness
+// #  define ENABLE_RGB_MATRIX_CYCLE_ALL                   // Full keyboard solid hue cycling through full gradient
+#endif
