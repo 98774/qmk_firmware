85177d1cdb7cf6ebc34242231470b7da814cdd3a
Jonathon Shirk Snyder <j.snyder98774@gmail.com>
2024-04-02 18:28:59 -0400

    -- v98774.0.0 --
    Fully configured kemap to test with

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/98774-RBG/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/98774-RBG/config.h
@@ -1,4 +1,6 @@
-/* This is the c configuration file for the keymap
+/*
+This is the c configuration file for the keymap
+
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 
@@ -41,83 +43,73 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #undef SOFT_SERIAL_PIN
 #endif
 
-#define SPLIT_MODS_ENABLE
-#define SPLIT_WATCHDOG_ENABLE
-//  * Reboot slave if no communication from master within timeout.
-//  * Helps resolve issue where both sides detect as slave using `SPLIT_USB_DETECT`
 #define USE_I2C
-#define SPLIT_WATCHDOG_TIMEOUT 3000
-//  * Maximum slave timeout when waiting for communication from master when using `SPLIT_WATCHDOG_ENABLE`
+//SPLIT KEYBOARD
+#define SPLIT_MODS_ENABLE //  * Reboot slave if no communication from master within timeout.
+#define SPLIT_WATCHDOG_ENABLE //  * Helps resolve issue where both sides detect as slave using `SPLIT_USB_DETECT`
+#define FORCED_SYNC_THROTTLE_MS 100 //  * Deadline for synchronizing data from master to slave when using the QMK-provided split transport.
+#define SPLIT_WATCHDOG_TIMEOUT 3000 //  * Maximum slave timeout when waiting for communication from master when using `SPLIT_WATCHDOG_ENABLE`
+#define SPLIT_TRANSPORT_MIRROR //  * Mirrors the master-side matrix on the slave when using the QMK-provided split transport.
+#define SPLIT_LAYER_STATE_ENABLE //  * Ensures the current layer state is available on the slave when using the QMK-provided split transport.
+#define SPLIT_LED_STATE_ENABLE //   * Ensures the current host indicator state (caps/num/scroll) is available on the slave when using the QMK-provided split transport.
+#define SPLIT_OLED_ENABLE
+#define SPLIT_WPM_ENABLE
 
-#define FORCED_SYNC_THROTTLE_MS 100
-//  * Deadline for synchronizing data from master to slave when using the QMK-provided split transport.
-
-//* `#define SPLIT_TRANSPORT_MIRROR
-//  * Mirrors the master-side matrix on the slave when using the QMK-provided split transport.
-
-#define SPLIT_LAYER_STATE_ENABLE
-//  * Ensures the current layer state is available on the slave when using the QMK-provided split transport.
-
-//#define SPLIT_LED_STATE_ENABLE
-//   * Ensures the current host indicator state (caps/num/scroll) is available on the slave when using the QMK-provided split transport.
-
-// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_BREATHING + 2
 // #define USB_MAX_POWER_CONSUMPTION 500
 
 #define RGB_DISABLE_AFTER_TIMEOUT 0 // number of ticks to wait until disabling effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 #define RGB_MATRIX_LED_PROCESS_LIMIT (46 + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
 #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
 
-#  define RGB_MATRIX_HUE_STEP 4
-#  define RGB_MATRIX_SAT_STEP 4
-#  define RGB_MATRIX_VAL_STEP 4
-#  define RGB_MATRIX_SPD_STEP 5
+#  define RGB_MATRIX_HUE_STEP 8
+#  define RGB_MATRIX_SAT_STEP 8
+#  define RGB_MATRIX_VAL_STEP 8
+#  define RGB_MATRIX_SPD_STEP 8
 
 #ifdef RGB_MATRIX_ENABLE
 // Enable animations
-// #  define ENABLE_RGB_MATRIX_ALPHAS_MODS                 // Static dual hue speed is hue for secondary hue
+#  define ENABLE_RGB_MATRIX_ALPHAS_MODS                 // Static dual hue speed is hue for secondary hue
 // #  define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN            // Static gradient top to bottom speed controls how much gradient changes
-// #  define ENABLE_RGB_MATRIXOGRADIENT_LEFT_RIGHT         // Static gradient left to right speed controls how much gradient changes
-// #  define ENABLE_RGB_MATRIX_BREATHING                   // Single hue brightness cycling animation
-//#  define ENABLE_RGB_MATRIX_BAND_SAT                    // Single hue band fading saturation scrolling left to right
-//#  define ENABLE_RGB_MATRIX_BAND_VAL                    // Single hue band fading brightness scrolling left to right
-//#//  define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT           // Single hue 3 blade spinning pinwheel fades saturation
-//# // define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL           // Single hue 3 blade spinning pinwheel fades brightness
-//#  define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT             // Single hue spinning spiral fades saturation
-//#  define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL             // Single hue spinning spiral fades brightness
-//#  define ENABLE_RGB_MATRIX_CYCLE_ALL                   // Full keyboard solid hue cycling through full gradient
-#  define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT            // Full gradient scrolling left to right
-//#  define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN               // Full gradient scrolling top to bottom
-//#  define ENABLE_RGB_MATRIX_CYCLE_OUT_IN                // Full gradient scrolling out to in
-//#  define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL           // Full dual gradients scrolling out to in
-// #  define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON      // Full gradent Chevron shapped scrolling left to right
-// #  define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL              // Full gradient spinning pinwheel around center of keyboard
-// #  define ENABLE_RGB_MATRIX_CYCLE_SPIRAL                      // Full gradient spinning spiral around center of keyboard
-// #  define ENABLE_RGB_MATRIX_DUAL_BEACON                 // Full gradient spinning around center of keyboard
-// #  define ENABLE_RGB_MATRIX_RAINBOW_BEACON              // Full tighter gradient spinning around center of keyboard
-// #  define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS           // Full dual gradients spinning two halfs of keyboard
-// #  define ENABLE_RGB_MATRIX_RAINDROPS                   // Randomly changes a single key's hue
-// #  define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS         // Randomly changes a single key's hue and saturation
-// #  define ENABLE_RGB_MATRIX_HUE_BREATHING               // Hue shifts up a slight ammount at the same time then shifts back
-//#  define ENABLE_RGB_MATRIX_HUE_PENDULUM                // Hue shifts up a slight ammount in a wave to the right then back to the left
-//#  define ENABLE_RGB_MATRIX_HUE_WAVE                          // Hue shifts up a slight ammount and then back down in a wave to the right
-// #  define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Required for the following two effects
-// #  define ENABLE_RGB_MATRIX_TYPING_HEATMAP               // How hot is your WPM!
-// #  define ENABLE_RGB_MATRIX_DIGITAL_RAIN                 // That famous computer simulation
-//#  define RGB_MATRIX_KEYPRESSES // reacts to keypresses, required for the remaining effects
-//#  define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+// #  define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT         // Static gradient left to right speed controls how much gradient changes
+#  define ENABLE_RGB_MATRIX_BREATHING                   // Single hue brightness cycling animation
+#  define ENABLE_RGB_MATRIX_BAND_SAT                    // Single hue band fading saturation scrolling left to right
+#  define ENABLE_RGB_MATRIX_BAND_VAL                    // Single hue band fading brightness scrolling left to right
+//# define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT           // Single hue 3 blade spinning pinwheel fades saturation
+//# define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL           // Single hue 3 blade spinning pinwheel fades brightness
+// # define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT             // Single hue spinning spiral fades saturation
+// # define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL             // Single hue spinning spiral fades brightness
+# define ENABLE_RGB_MATRIX_CYCLE_ALL                   // Full keyboard solid hue cycling through full gradient
+# define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT            // Full gradient scrolling left to right
+//# define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN               // Full gradient scrolling top to bottom
+//# define ENABLE_RGB_MATRIX_CYCLE_OUT_IN                // Full gradient scrolling out to in
+//# define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL           // Full dual gradients scrolling out to in
+# define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON      // Full gradent Chevron shapped scrolling left to right
+//# define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL              // Full gradient spinning pinwheel around center of keyboard
+//# define ENABLE_RGB_MATRIX_CYCLE_SPIRAL                      // Full gradient spinning spiral around center of keyboard
+//# define ENABLE_RGB_MATRIX_DUAL_BEACON                 // Full gradient spinning around center of keyboard
+//# define ENABLE_RGB_MATRIX_RAINBOW_BEACON              // Full tighter gradient spinning around center of keyboard
+//# define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS           // Full dual gradients spinning two halfs of keyboard
+//# define ENABLE_RGB_MATRIX_RAINDROPS                   // Randomly changes a single key's hue
+//# define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS         // Randomly changes a single key's hue and saturation
+//# define ENABLE_RGB_MATRIX_HUE_BREATHING               // Hue shifts up a slight ammount at the same time then shifts back
+//# define ENABLE_RGB_MATRIX_HUE_PENDULUM                // Hue shifts up a slight ammount in a wave to the right then back to the left
+//# define ENABLE_RGB_MATRIX_HUE_WAVE                          // Hue shifts up a slight ammount and then back down in a wave to the right
+//# define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Required for the following two effects
+//# define ENABLE_RGB_MATRIX_TYPING_HEATMAP               // How hot is your WPM!
+//# define ENABLE_RGB_MATRIX_DIGITAL_RAIN                 // That famous computer simulation
+//# define RGB_MATRIX_KEYPRESSES // reacts to keypresses, required for the remaining effects
+//# define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
 #ifdef RGB_MATRIX_KEYPRESSES
-#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE       // Pulses keys hit to hue & value then fades value out
+// #  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE       // Pulses keys hit to hue & value then fades value out
 //#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE              // Static single hue pulses keys hit to shifted hue then fades to current hue
 // #  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE         // Hue & value pulse near a single key hit then fades value out
 // #  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE    // Hue & value pulse near multiple key hits then fades value out
-#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS        // Hue & value pulse the same column and row of a single key hit then fades value out
+//#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS        // Hue & value pulse the same column and row of a single key hit then fades value out
 // #  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS   // Hue & value pulse the same column and row of multiple key hits then fades value out
-#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS        // Hue & value pulse away on the same column and row of a single key hit then fades value out
+//#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS        // Hue & value pulse away on the same column and row of a single key hit then fades value out
 // #  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS   // Hue & value pulse away on the same column and row of multiple key hits then fades value out
-#  define ENABLE_RGB_MATRIX_SPLASH                      // Full gradient & value pulse away from a single key hit then fades value out
+// #  define ENABLE_RGB_MATRIX_SPLASH                      // Full gradient & value pulse away from a single key hit then fades value out
 // #  define ENABLE_RGB_MATRIX_MULTISPLASH                 // Full gradient & value pulse away from multiple key hits then fades value out
 // #  define ENABLE_RGB_MATRIX_SOLID_SPLASH                // Hue & value pulse away from a single key hit then fades value out
 #  define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH           // Hue & value pulse away from multiple key hits then fades value out
